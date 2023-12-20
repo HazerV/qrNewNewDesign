7f8e0381910610c151123b29df9e3261e35e1497
HazerV <krotkov2004@icloud.com>
2023-12-20 18:25:09 +0400

    nnn

--- Components/ProductItem/Zoom/ZoomFunction.jsx
+++ Components/ProductItem/Zoom/ZoomFunction.jsx
@@ -1,4 +1,4 @@
-import { GestureDetector, Gesture,  GestureHandlerRootView} from 'react-native-gesture-handler';
+import { GestureDetector, State, Gesture,  GestureHandlerRootView} from 'react-native-gesture-handler';
 import Animated, {
     useSharedValue,
     useAnimatedStyle,
@@ -6,8 +6,6 @@ import Animated, {
     runOnJS,
 } from 'react-native-reanimated';
 import { impactAsync, ImpactFeedbackStyle } from 'expo-haptics';
-import {useRef} from "react";
-import {Dimensions} from "react-native";
 
 const ZoomFunction =  ({children}) => {
 
@@ -15,7 +13,6 @@ const ZoomFunction =  ({children}) => {
     const pressed = useSharedValue(false);
     const scale = useSharedValue(1);
     const contextScale = useSharedValue(1);
-
     const pinchGesture = Gesture.Pinch()
         .onUpdate((event) => {
             scale.value = contextScale.value * event.scale
@@ -26,21 +23,30 @@ const ZoomFunction =  ({children}) => {
             contextScale.value = scale.value;
         })
     const longPressGesture = Gesture.LongPress()
-        .onEnd(() => {
-            scale.value = 1
-            contextScale.value = scale.value
-        })
         .onStart((event) => {
             console.log('coords', event)
             runOnJS(impactAsync)(ImpactFeedbackStyle.Medium)
-            offset.value.x = (event.absoluteX / 2) ;
-            offset.value.y = (event.absoluteY  / 2) ;
-            scale.value = 2
-            contextScale.value = 2
-        });
+            offset.value = {
+                x: event.x,
+                y: event.y
+            }
+            // scale.value = 2
+            // contextScale.value = 2
+        })
+        .onEnd(() => {
+            offset.value = {
+                x: 0,
+                y: 0
+            }
+            // scale.value = 1
+            // contextScale.value = scale.value
+        })
     const panGesture = Gesture.Pan()
         .averageTouches(true)
-        .onStart(() => (pressed.value = true))
+        .onStart((e) => {
+            pressed.value = true
+            console.log('pan', e)
+        } )
         .onUpdate((e) => {
             offset.value = {
                 x: e.translationX,
@@ -49,8 +55,8 @@ const ZoomFunction =  ({children}) => {
         })
         .onEnd(() => {
             offset.value = withSpring({x: 0,y: 0})
-            pressed.value = false
-            scale.value = 1.5
+            // pressed.value = false
+            scale.value = 1
         })
     const composedGestures =  Gesture.Simultaneous(longPressGesture, Gesture.Simultaneous(panGesture, pinchGesture))
     const animatedStyles = useAnimatedStyle(() => {
@@ -65,7 +71,7 @@ const ZoomFunction =  ({children}) => {
     });
     return  (
         <GestureHandlerRootView>
-            <GestureDetector gesture={composedGestures}>
+            <GestureDetector gesture={longPressGesture}>
                 <Animated.View style={animatedStyles}>
                     {children}
                 </Animated.View>
